0b19a1a9cde3b7125f64453f30dadef20604d982
Dennish90 <dennis.holm@chasacademy.se>
2024-10-29 13:56:13 +0100

    Update Board.cpp
    Added new function to place ships

--- Board.cpp
+++ Board.cpp
@@ -1,139 +1,129 @@
 #include <iostream>
-#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <vector>
 
-class Board {
-public:
+using namespace std;
+
+class Matrix{
+    public:
     enum CellStatus { Water, Miss, Hit, Ship };
-
-    Board() {
-        // Creates the pacific ocean
-        for (int i = 0; i < 15; ++i)
-        {
-            for (int j = 0; j < 15; ++j) 
-            {
-                grid[i][j] = Water;
-            }
-        }
-    }
-
-    // Simple ship placer for testing
-    void placeShip(int x, int y) 
-    {
-        if (isValidPosition(x, y)) 
-        {
-            grid[x][y] = Ship;
-        }
-    }
-
-    // Fire the cannons!
-    bool fireCannon(int x, int y) {
-        if (!isValidPosition(x, y)) return false;
-
-        if (grid[x][y] == Ship) 
-        {
-            grid[x][y] = Hit;
-            return true;  // Hit
-        } 
-        else if (grid[x][y] == Water) 
-        {
-            grid[x][y] = Miss;
-            return false; // Miss
-        }
-        return false; // Already hit
-    }
-
-    // Metod för att visa spelplanen
-    void displayPlayer() const {
-        std::cout << "   ";
-        for (int i = 0; i < 15; i++)
-        {
-            if ( i >= 8 ) std::cout << i + 1 << " ";
-            else std::cout << i + 1 << "  ";
-        }
-        std::cout << std::endl;
-
-        for (int i = 0; i < 15; ++i) 
-        {
-            std::cout << (char)(i + 65) << "  ";
-            for (int j = 0; j < 15; ++j) 
-            {
-                if (grid[i][j] == Water) std::cout << "~  ";
-                else if (grid[i][j] == Miss) std::cout << "O  ";
-                else if (grid[i][j] == Hit) std::cout << "X  ";
-                else if (grid[i][j] == Ship) std::cout << "#  ";
-            }
-            std::cout << std::endl;
-        }
-    }
-
-    void displayOpponent() const {
-        std::cout << "   ";
-        for (int i = 0; i < 15; i++)
-        {
-            if ( i >= 8 ) std::cout << i + 1 << " ";
-            else std::cout << i + 1 << "  ";
-        }
-        std::cout << std::endl;
-
-        for (int i = 0; i < 15; ++i) 
-        {
-            std::cout << (char)(i + 65) << "  ";
-            for (int j = 0; j < 15; ++j) 
-            {
-                if (grid[i][j] == Water) std::cout << "~  ";
-                else if (grid[i][j] == Miss) std::cout << "O  ";
-                else if (grid[i][j] == Hit) std::cout << "X  ";
-                else if (grid[i][j] == Ship) std::cout << "~  ";
-            }
-            std::cout << std::endl;
-        }
-    }
-
-    // Controls if position is inside the pacific ocean
-    bool isValidPosition(int x, int y) const {
-        return x >= 0 && x < 15 && y >= 0 && y < 15;
-    }
-    CellStatus grid[15][15];  // 15x15 Pacific ocean
-
+    static const int xRow = 15;
+    static const int yRow = 15;
+    int grid[xRow][yRow];
 };
 
-// My little playground to test to sink ships
-int main() {
-    Board board;
+class Ship
+{
+    public:
+        string name;
+        int shipId;
+        int size;
+        vector<int> positionX;
+        vector<int> positionY;
+        bool isSunk;
+};
 
-    char aimX;
-    int aimY;
+bool isvalid(int x, int y)
+{
+    if(x >= 0 && x < 15 && y >= 0 && y < 15) { return true; }
+    else return false;
+}
 
-    // Place a 3 tile ship
-    board.placeShip(5, 7);
-    board.placeShip(5, 8);
-    board.placeShip(5, 9);
+void buildShip(int size, Matrix& matrix)
+{
+    // need 5 ships;
+    char placeShipX;
+    int placeShipY;
+    char direction;
+            cin >> placeShipX;
+            cin >> placeShipY;
+            cin >> direction;
 
-    // Place a 5 tile ship
-    board.placeShip( 9, 3);
-    board.placeShip(10, 3);
-    board.placeShip(11, 3);
-    board.placeShip(12, 3);
-    board.placeShip(13, 3);
-    do
+            if (direction == 'E')
+            {
+                for (int i = placeShipY; i < placeShipY + size; i++)
+                {
+                    matrix.grid[placeShipX - 65][i - 1] = Matrix::Ship;
+                }
+            }
+            else if (direction == 'S')
+            {
+                for (int i = placeShipX; i < placeShipX + size; i++)
+                {
+                    matrix.grid[i - 65][placeShipY - 1] = Matrix::Ship;
+                }
+            }
+            isvalid(placeShipX, placeShipY);  
+}
+
+void intOcean(Matrix& matrix)
+{
+    for (int i = 0; i < 15; i++)
     {
-        // Print game display
-        std::cout << "-==============-Opponents side-==============-" << std::endl;
-        board.displayOpponent();
-        std::cout << "-===============-Players side-===============-" << std::endl;
-        board.displayPlayer();
+        for (int j = 0; j < 15; j++)
+        {
+            matrix.grid[i][j] = Matrix::Water;
+        }
+    }
+}
 
-        // Enter coordinates and fire!
-        std::cout << "Aim: ";
-        std::cin >> aimX;
-        std::cin >> aimY;
-        board.fireCannon((int)(aimX - 65), aimY - 1);
-        // Clear the console before next round
-        system("CLS");
-        if (board.grid[(int)(aimX - 65)][aimY - 1] == Board::Hit) std::cout << "Hit!" << std::endl;
-        else std::cout << "Miss!" << std::endl;
+void displayOceanPlayer(Matrix& matrix)
+{
+    cout << "-==================-Player-==================-" << endl;
+    cout << " ";
+    for (int i = 0; i < 15; i++)
+    {
+        if ( i >= 9 ) cout << " " << i + 1;
+        else cout << "  " << i + 1;
+    }
+    cout << endl;
+    for (int i = 0; i < 15; i++)
+    {
+        cout << (char)(i + 65);
+        for (int j = 0; j < 15; j++)
+        {
+            if (matrix.grid[i][j] == Matrix::Water) cout << "  ~";  // DET ÄR BARA VATTEN!
+            if (matrix.grid[i][j] == Matrix::Hit) cout << "  X";    // DET ÄR TRÄFF!
+            if (matrix.grid[i][j] == Matrix::Miss) cout << "  0";   // DET ÄR MISS!
+            if (matrix.grid[i][j] == Matrix::Ship) cout << "  #";   // DET ÄR ETT SKEPP!
+        }
+        cout << endl;                                               // DET ÄR RADBRYTNING!
+    }
+}
 
-    } while (1);
+void displayOceanOpponent(Matrix& matrix)
+{
+    cout << "-=================-Opponent-=================-" << endl;
+    cout << " ";
+    for (int i = 0; i < 15; i++)
+    {
+        if ( i >= 9 ) cout << " " << i + 1;
+        else cout << "  " << i + 1;
+    }
+    cout << endl;
+    for (int i = 0; i < 15; i++)
+    {
+        cout << (char)(i + 65);
+        for (int j = 0; j < 15; j++)
+        {
+            if (matrix.grid[i][j] == Matrix::Water) cout << "  ~";  // DET ÄR BARA VATTEN!
+            if (matrix.grid[i][j] == Matrix::Hit) cout << "  X";    // DET ÄR TRÄFF!
+            if (matrix.grid[i][j] == Matrix::Miss) cout << "  0";   // DET ÄR MISS!
+            if (matrix.grid[i][j] == Matrix::Ship) cout << "  ~";   // DET ÄR ETT SKEPP!
+        }
+        cout << endl;                                               // DET ÄR RADBRYTNING!
+    }
+}
+
+int main()
+{
+    Matrix ocean;
+    intOcean(ocean);
+    displayOceanOpponent(ocean);
+    displayOceanPlayer(ocean);
+    displayOceanOpponent(ocean);
+    displayOceanPlayer(ocean);
 
     return 0;
 }
